0f55d9e176b6c45edfdde26f619eadf61f47c637
Almhar <almhar.ilao@tnsinc.com>
2023-01-09 15:24:34 +0800

    change 1
    change 2
    
    change 3

--- src/App.jsx
+++ src/App.jsx
@@ -17,17 +17,17 @@ function App() {
       <img src={starBucks} alt="" style={{ display: "none" }} />
 
       <div>
-        {/* header */}
+        {/* header change 1*/}
 
         <Header userLogin={userLogin}/>
 
         <div className="mainPage-wrapper">
-          <div className="App_post_contain">{/* post here */}
+          <div className="App_post_contain">{/* post here change 2*/}
             <Posts userLogin={userLogin}/>
             {/* {renderPost} */}
           </div>
 
-          {/* Instagram profile clone */}
+          {/* Instagram profile clone change 3*/}
           <InstagramEmbed
             url='https://www.instagram.com/p/CO9GxwaJBZp/'
             maxWidth={320}
